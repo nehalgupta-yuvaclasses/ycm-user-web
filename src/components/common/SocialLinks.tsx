a8ad6e136919a5c20768ceed7b0e9d3f91608728
Shivam Kumar <53265299+shivamksharma@users.noreply.github.com>
2026-04-20 17:07:29 +0530

    Initial Commit

--- src/components/common/SocialLinks.tsx
+++ src/components/common/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Linkedin, MessageCircle, Send, Twitter, Youtube } from 'lucide-react';
+import { FaWhatsapp, FaTelegram, FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { cn } from '@/lib/utils';
 import { type SocialsData, isSocialLink } from '@/services/socialsService';
 
@@ -11,49 +11,49 @@ const socialItems = [
   {
     key: 'whatsapp',
     label: 'WhatsApp',
-    icon: MessageCircle,
+    icon: FaWhatsapp,
     href: (socials: SocialsData) => socials.whatsapp,
     hoverClass: 'hover:bg-emerald-600 hover:border-emerald-600',
   },
   {
     key: 'telegram',
     label: 'Telegram',
-    icon: Send,
+    icon: FaTelegram,
     href: (socials: SocialsData) => socials.telegram,
     hoverClass: 'hover:bg-sky-500 hover:border-sky-500',
   },
   {
     key: 'instagram',
     label: 'Instagram',
-    icon: Instagram,
+    icon: FaInstagram,
     href: (socials: SocialsData) => socials.instagram,
     hoverClass: 'hover:bg-pink-500 hover:border-pink-500',
   },
   {
     key: 'facebook',
     label: 'Facebook',
-    icon: Facebook,
+    icon: FaFacebookF,
     href: (socials: SocialsData) => socials.facebook,
     hoverClass: 'hover:bg-blue-600 hover:border-blue-600',
   },
   {
     key: 'twitter',
     label: 'X',
-    icon: Twitter,
+    icon: FaTwitter,
     href: (socials: SocialsData) => socials.twitter,
     hoverClass: 'hover:bg-zinc-900 hover:border-zinc-900',
   },
   {
     key: 'youtube',
     label: 'YouTube',
-    icon: Youtube,
+    icon: FaYoutube,
     href: (socials: SocialsData) => socials.youtube,
     hoverClass: 'hover:bg-red-600 hover:border-red-600',
   },
   {
     key: 'linkedin',
     label: 'LinkedIn',
-    icon: Linkedin,
+    icon: FaLinkedinIn,
     href: (socials: SocialsData) => socials.linkedin,
     hoverClass: 'hover:bg-sky-700 hover:border-sky-700',
   },
